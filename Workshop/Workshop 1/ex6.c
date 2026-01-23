d8c4e5e256a70df195d227543f0a53f40ed07d1c
maidangthebao22 <baomaidang2006@gmail.com>
2026-01-23 17:20:15 +0700

    Workshop 1

--- Workshop/Workshop 1/ex6.c
+++ Workshop/Workshop 1/ex6.c
@@ -3,12 +3,21 @@
 int main() {
 	float cc, gk, ck, tongket;
 	char diemchu;
-	printf("Nhap diem chuyen can");
-	scanf("%f", &cc);
-	printf("Nhap diem giua ky");
-	scanf("%f", &gk);
-	printf("Nhap diem cuoi ky");
-	scanf("%f", &ck);
+	printf("Nhap diem chuyen can: ");
+	if (scanf("%f", &cc) !=1 || cc < 0 || cc > 10){
+		printf("Nhap sai!\n");
+		return 0;
+	}
+	printf("Nhap diem giua ky: ");
+	if (scanf("%f", &gk) !=1 || gk < 0 || gk >10){
+		printf("Nhap sai!\n");
+		return 0;
+	} 
+	printf("Nhap diem cuoi ky: ");
+	if (scanf("%f", &ck) !=1 || ck < 0 || ck > 10){
+		printf("Nhap sai!\n");
+		return 0;
+	}
 	tongket = cc*0.1 + gk*0.3 + ck*0.6;
 	if (tongket >= 8){
 		diemchu = 'A';
@@ -25,6 +34,7 @@ int main() {
 	else{
 		diemchu = 'F';
 	}
+	printf ("Diem Tong ket: %c\n", diemchu);
 	if (cc < 4 || gk < 4 || ck < 4 || diemchu == 'F'){
 		printf ("KHONG DU DIEU KIEN TOT NGHIEP");
 	}
